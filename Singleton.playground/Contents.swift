7fefadd643ef18132b9e6161e8ad7040e885f883
Galih Samodra Wicaksono <>
2022-02-11 20:45:10 +0700

    feat(login): add login module

--- Singleton.playground/Contents.swift
+++ Singleton.playground/Contents.swift
@@ -9,3 +9,25 @@ class ApiClient {
 class MockApiClient: ApiClient {
     
 }
+
+// Login Module
+struct User {}
+protocol LoginClient {
+    func loggedIn(completion: (User) -> Void)
+}
+
+extension ApiClient: LoginClient {
+    func loggedIn(completion: (User) -> Void) {}
+}
+
+class LoginService {
+    
+    var loggedIn: (((User) -> Void) -> Void)?
+    
+    
+    func login() {
+        loggedIn? { user in
+            
+        }
+    }
+}
